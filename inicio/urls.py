0b9fe7fbd98ddede52f227d627e912cb98c6bf3e
Raishin0 <julian.magnabosco@gmail.com>
2025-03-28 22:19:10 -0300

    pruebas visuales

--- inicio/urls.py
+++ inicio/urls.py
@@ -2,13 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('form/', views.form),
-    path('childs/', views.childs, name='childs'),
-    path('ping/', views.ping),
-    path('lines/', views.lines),
-    path('line/<int:id>', views.line),
-    path('hello/<str:name>', views.hello),
+    path('', views.base),
     path("chat/", views.chat, name="index"),
     path("chat/<str:room_name>/", views.room, name="room"),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -4,43 +4,10 @@ from .models import Line
 from .forms import LineForm
 
 # Create your views here.
-def index(request):
-    title="Index Variable"
-    lines = list(Line.objects.values())
-    emptyList = len(lines)<=0
-    return render(request,'index.html',{
-        "title":title,
-        "emptyList":emptyList
-    })
-
-def childs(request):
-    return render(request,'child.html')
-
-def form(request):
-    if request.method == "GET":
-        return render(request,'form.html',{
-            'form':LineForm()
-        })
-    else:
-        Line.objects.create(name=request.POST["name"],code=request.POST["code"])
-        return redirect("/form/")
-
-def ping(request):
-    return HttpResponse("pong")
-
-
-def hello(request,name):
-    return HttpResponse("Hello %s" % name)
-
-
-def lines(request):
-    lines = list(Line.objects.values())
-    return JsonResponse(lines,safe=False)
-
-def line(request,id):
-    line = get_object_or_404(Line,id=id)
-    return HttpResponse(line.name)
-
+def base(request):
+    options = ({"code":"OC","name":""},
+               {"code":"P","name":"Pediatria"})
+    return render(request, 'base.html', {"options": options})
 
 def chat(request):
     return render(request, "chat.html")
